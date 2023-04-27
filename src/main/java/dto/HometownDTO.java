4066335b33766a575255e48178785d95fe246de4
swkimsw <swkimsw3746@gmail.com>
2023-04-27 11:58:39 +0900

    승우
    확인 부탁드려요

--- src/main/java/dto/HometownDTO.java
+++ src/main/java/dto/HometownDTO.java
@@ -8,10 +8,9 @@ public class HometownDTO {
 	private Timestamp reg_date;
 	private Timestamp mod_date;
 	private Timestamp del_date;
-
-	public HometownDTO() {
-	}
-
+	
+	
+	public HometownDTO() {}
 	public HometownDTO(int code, String name, Timestamp reg_date, Timestamp mod_date, Timestamp del_date) {
 		this.code = code;
 		this.name = name;
@@ -19,45 +18,36 @@ public class HometownDTO {
 		this.mod_date = mod_date;
 		this.del_date = del_date;
 	}
-
+	
 	public int getCode() {
 		return code;
 	}
-
 	public void setCode(int code) {
 		this.code = code;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Timestamp getReg_date() {
 		return reg_date;
 	}
-
 	public void setReg_date(Timestamp reg_date) {
 		this.reg_date = reg_date;
 	}
-
 	public Timestamp getMod_date() {
 		return mod_date;
 	}
-
 	public void setMod_date(Timestamp mod_date) {
 		this.mod_date = mod_date;
 	}
-
 	public Timestamp getDel_date() {
 		return del_date;
 	}
-
 	public void setDel_date(Timestamp del_date) {
 		this.del_date = del_date;
 	}
-
+	
 }
